03e64b7e45b21494ea70f6171e4e1a1633604976
Diego Cabrera <90268530+Diegcor@users.noreply.github.com>
2024-05-09 12:14:43 -0600

    Moved buffer program into the main loop so that the vertex array can be updated every cycle

--- Main.cpp
+++ Main.cpp
@@ -45,66 +45,74 @@ int main()
 
 	//creating type data
 	GLfloat typeData[] = {
-		0.02f, 1.0f, 0.0f, 0.0f
+		0.02f, 1.0f, 0.0f, 0.0f,
+		0.04f, 0.0f, 1.0f, 0.0f
 	};
 
 	// Initilising vertex and index buffers
 	GLfloat vertices[13 * 6 * NUM_PARTICLES_MAIN] = {};
 	GLuint indices[21 * NUM_PARTICLES_MAIN] = {};
 
-	Particle2D* particles = new Particle2D[NUM_PARTICLES_MAIN]{ {0.0f,0.0f,0}, {0.5f, 0.5f, 0} };
-
-	fillVertices(particles, vertices, indices, NUM_PARTICLES_MAIN, typeData, 4);
+	Particle2D* particles = new Particle2D[NUM_PARTICLES_MAIN]{};
 
 	Shader shaderProgram("particle2d.vert", "default.frag");
-
-	// Generates Vertex Array Object and binds it
-	VAO VAO1;
-	VAO1.Bind();
-
-	// Generates Vertex Buffer Object and links it to vertices
-	VBO VBO1(vertices, sizeof(vertices));
-
-	// Generates Element Buffer Object and links it to indices
-	EBO EBO1(indices, sizeof(indices));
-
-	// Links VBO to VAO
-	VAO1.LinkAttrib(VBO1, 0, 2, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 4, GL_FLOAT, 6 * sizeof(float), (void*)(2*sizeof(float)));
-
-	// Unbind all
-	VAO1.Unbind();
-	VBO1.Unbind();
-	EBO1.Unbind();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
-		// Specify the color of the background
+		//background
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
 		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
-		// Tell OpenGL which Shader Program we want to use
-		shaderProgram.Activate();
-		glUniform1f(uniID, 0.0f);
-		// Bind the VAO so OpenGL knows to use it
+
+
+		//Simulate here
+
+		fillVertices(particles, vertices, indices, NUM_PARTICLES_MAIN, typeData, 4);
+
+		// Generates Vertex Array Object and binds it
+		VAO VAO1;
 		VAO1.Bind();
-		// Draw primitives, number of indices, datatype of indices, index of indices
+
+		// Generates Vertex Buffer Object and links it to vertices
+		VBO VBO1(vertices, sizeof(vertices));
+
+		// Generates Element Buffer Object and links it to indices
+		EBO EBO1(indices, sizeof(indices));
+
+		// Links VBO to VAO
+		VAO1.LinkAttrib(VBO1, 0, 2, GL_FLOAT, 6 * sizeof(float), (void*)0);
+		VAO1.LinkAttrib(VBO1, 1, 4, GL_FLOAT, 6 * sizeof(float), (void*)(2 * sizeof(float)));
+
+		// Unbind all
+		VAO1.Unbind();
+		VBO1.Unbind();
+		EBO1.Unbind();
+
+		shaderProgram.Activate();
+
+		//Set zoom level here
+		glUniform1f(uniID, 0.0f);
+
+		// Bind the VAO
+		VAO1.Bind();
+	
 		glDrawElements(GL_TRIANGLE_STRIP, 21*NUM_PARTICLES_MAIN, GL_UNSIGNED_INT, 0);
-		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
+
 		// Take care of all GLFW events
 		glfwPollEvents();
+
+		// effectivly reset all the buffers
+		VAO1.Delete();
+		VBO1.Delete();
+		EBO1.Delete();
 	}
 
 
-
-	// Delete all the objects we've created
-	VAO1.Delete();
-	VBO1.Delete();
-	EBO1.Delete();
 	shaderProgram.Delete();
 
 	// Delete window before ending the program
