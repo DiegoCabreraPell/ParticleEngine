395cbbf56465796686fd7fdfc5cd8987a90478eb
Diego Cabrera <90268530+Diegcor@users.noreply.github.com>
2024-06-27 14:03:37 -0600

    removed old particle class and siomulate step file

--- simulateStep.h
+++
@@ -1,7 +0,0 @@
-#ifndef SIMULATE_STEP_H
-#define SIMULATE_STEP_H
-#include"particle2DClass.h"
-
-void simulateStep(Particle2D** particles, int numParticles, GLfloat* typeData, int dataComps, GLfloat* typeMatrix, int numTypes);
-
-#endif
