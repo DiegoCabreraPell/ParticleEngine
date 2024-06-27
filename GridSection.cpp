7743c8f642faf0ec8b51cc6571e57ab89228745b
Diego Cabrera <90268530+Diegcor@users.noreply.github.com>
2024-06-27 14:03:05 -0600

    fixed typo in collision check

--- GridSection.cpp
+++ GridSection.cpp
@@ -138,7 +138,7 @@ void GridSection::handleCollsions(collisionResolver resolver, float* sizes)
 				if (dy == 0.0f)
 					dy = 0.00001;
 
-				distance = sqrtf(powf(dy, 2) + powf(dx, 2)) - sizes[i->type] - sizes[j->type];
+				distance = sqrtf(powf(dy, 2) + powf(dx, 2)) - sizes[i->type] - sizes[p->type];
 
 				if (distance < 0)
 					resolver(*i, *p, distance);
